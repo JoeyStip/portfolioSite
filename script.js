ab04b46d7a8ed83f9007983432b20489a541e65e
Joseph Stipich <stipichj@gmail.com>
2024-06-18 11:52:06 -0400

    add sticky nav bar and extend page plus jquery tweaks

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ $(document).ready(()=>{
 
 $("img")
     .mouseenter((event)=>{
-    //console.log("#" + event.target.id + "Label");
-    $("#" + event.target.id + "Label").css("opacity", 1)
-    //.attr("opacity", 1.0)
+        // console.log(event)
+        // cool idea: list projects out and have the whole background change to a screenshot
+        // of the project site when you mouseover its item on the list
+        let width = event.target.width;
+        let halfWidth = Math.round(width/2);
+
+        $("#" + event.target.id + "Label")
+            .css("opacity", 1)
+            .css("transform", "translate(" + halfWidth + "px,-50px)" )
+            .css("width", width);
 })
     .mouseleave((event)=>{
-        $("#" + event.target.id + "Label").css("opacity", 0)
+        $("#" + event.target.id + "Label").css("opacity", 0);
+    })
+
+$(".projectPreview")
+    .mouseenter((event)=>{
+        console.log(event)
     })
